b9ada63a11dde54c3019e516ae3b365a46b96c44
Quskia <ad@tarbase.com>
2023-02-21 16:38:09 +0000

    split terraform

--- infra/main.tf
+++ infra/main.tf
@@ -29,8 +29,6 @@ resource "azurerm_storage_container" "terraformbackend" {
   container_access_type = "private"
 }
 
-
-
 resource "azurerm_resource_group" "discBotGPT" {
   name     = "discBotGPT"
   location = "West Europe"
@@ -43,73 +41,3 @@ resource "azurerm_virtual_network" "myVnet" {
   resource_group_name = azurerm_resource_group.discBotGPT.name
 }
 
-
-resource "azurerm_subnet" "mySubnet" {
-  name                 = "mySubnet"
-  resource_group_name  = azurerm_resource_group.discBotGPT.name
-  virtual_network_name = azurerm_virtual_network.myVnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-
-# Create a network interface with public address
-
-resource "azurerm_network_interface" "myNic" {
-  name                = "myNic"
-  location            = azurerm_resource_group.discBotGPT.location
-  resource_group_name = azurerm_resource_group.discBotGPT.name
-
-  ip_configuration {
-    name                          = "myNicConfiguration"
-    subnet_id                     = azurerm_subnet.mySubnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.myPublicIp.id
-  }
-}
-
-resource "azurerm_public_ip" "myPublicIp" {
-  name                = "myPublicIp"
-  location            = azurerm_resource_group.discBotGPT.location
-  resource_group_name = azurerm_resource_group.discBotGPT.name
-  allocation_method   = "Dynamic"
-}
-
-
-
-
-
-resource "azurerm_linux_virtual_machine" "myVM" {
-  name                = "VMDiscbotGPT"
-  resource_group_name = azurerm_resource_group.discBotGPT.name
-  location            = azurerm_resource_group.discBotGPT.location
-  size                = "Standard_B1s"
-  admin_username      = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.myNic.id,
-  ]
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("VMPriv.pem.pub")
-    
-
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-}
-
-
-
-output "public_ip_address" {
-  value = azurerm_public_ip.myPublicIp.ip_address
-}
---
+++ infra_app/app.tf
@@ -0,0 +1,84 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "discBotGPT"
+    storage_account_name = "storagedisbotgptvoice"
+    container_name       = "terraformbackend"
+    key                  = "terraformVM.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+# Create a network interface with public address
+
+resource "azurerm_network_interface" "myNic" {
+  name                = "myNic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "myNicConfiguration"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.myPublicIp.id
+  }
+}
+
+resource "azurerm_public_ip" "myPublicIp" {
+  name                = "myPublicIp"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_linux_virtual_machine" "myVM" {
+  name                = "VMDiscbotGPT"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.myNic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("VMPriv.pem.pub")
+    
+
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
+output "public_ip_address" {
+  value = azurerm_public_ip.myPublicIp.ip_address
+}
+
+#create var with resource group name and location
+variable "resource_group_name" {
+  type = string
+  default = "discBotGPT"
+}
+variable "location" {
+  type = string
+  default = "West Europe"
+}
+
+variable "subnet_id" {
+  type = string
+  default = "/subscriptions/a1dac0b3-167a-44d8-b506-b590072031f7/resourceGroups/discBotGPT/providers/Microsoft.Network/networkInterfaces/myNic/ipConfigurations/myNicConfiguration"
+}
