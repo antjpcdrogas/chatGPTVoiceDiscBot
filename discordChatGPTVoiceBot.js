6f67e297664d91f43c6a33b5029dde880df56e32
Quskia <ad@tarbase.com>
2023-04-25 18:41:47 +0100

    [2.1.1]

--- discordChatGPTVoiceBot.js
+++ discordChatGPTVoiceBot.js
@@ -373,7 +373,7 @@ console.log('Package version: ' + VERSION);
     console.log("Ready to go!");
     triggerRandomly();
     console.log("--------------------------------------------------")
-    console.log(speech_key)
+
 
 
 //get number of members in the voice channel
