17603707cec83edffa236ad17ee2677c61dd4f1e
Quskia <ad@tarbase.com>
2023-10-20 22:57:24 +0100

    [v3.0.0]

--- discordChatGPTVoiceBot.js
+++ discordChatGPTVoiceBot.js
@@ -210,7 +210,6 @@ function saveTextStream(textToSpeak, callback) {
 
 
 
-
 async function chatgpt_start(){
     console.log("Starting bot...")
         const configuration = new Configuration({ //TODO: add fine-tuning and custom model
