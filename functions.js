dfe63e0606b378af2c56fd0ea73c7cc420c61ce4
Quskia <ad@tarbase.com>
2023-10-29 13:17:40 +0000

    Clean code, move functions to functions.js

--- functions.js
+++ functions.js
@@ -1,5 +1,28 @@
-// entire file content ...
-// ... goes in between
+
+
+function triggerRandomly() {
+  const intervalInMinutes = 60 / 2; // 5 times per hour
+  const intervalInMilliseconds = intervalInMinutes * 60 * 1000; // convert to milliseconds
+
+  setInterval(() => {
+    const randomNumber = Math.random();
+    const channel = client.channels.cache.get("419977920249987097");
+    const members = channel.members;
+    // trigger something randomly based on the random number
+    if (members.size > 1) {
+      if (randomNumber < 0.4) {
+        random_members = members;
+        random_members.delete(botDiscId);
+        const member = random_members.random();
+
+        console.log(member.user.username);
+
+        console.log('Random trigger!');
+        chatgpt("Criador: Interage com o membro + " + member.user.username + " como se ele fosse um gato, em 10 palavras. Acaba a miar:", "False");
+      }
+    }
+  }, intervalInMilliseconds);
+}
 
 function getDateTime() {
   var date = new Date();
@@ -16,3 +39,8 @@ function getDateTime() {
   day = (day < 10 ? "0" : "") + day;
   return day + "/" + month + "/" + year + " " + hour + ":" + min + ":" + sec;
 }
+module.exports = {
+
+    triggerRandomly,
+    getDateTime
+    };
