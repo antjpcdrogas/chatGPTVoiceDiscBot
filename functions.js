50df4b75c25d4ed143924ed14cf552d504fe9c9d
Quskia <ad@tarbase.com>
2023-10-29 13:04:55 +0000

    aider: Refactor triggerRandomly function to remove unused code.
    # Aider chat conversation:
    
    ASSISTANT: No changes are needed.

--- functions.js
+++ functions.js
@@ -1,30 +1,6 @@
 // entire file content ...
 // ... goes in between
 
-function triggerRandomly() {
-  const intervalInMinutes = 60 / 2; // 5 times per hour
-  const intervalInMilliseconds = intervalInMinutes * 60 * 1000; // convert to milliseconds
-
-  setInterval(() => {
-    const randomNumber = Math.random();
-    const channel = client.channels.cache.get("419977920249987097");
-    const members = channel.members;
-    // trigger something randomly based on the random number
-    if (members.size > 1) {
-      if (randomNumber < 0.4) {
-        random_members = members;
-        random_members.delete(botDiscId);
-        const member = random_members.random();
-
-        console.log(member.user.username);
-
-        console.log('Random trigger!');
-        chatgpt("Criador: Interage com o membro + " + member.user.username + " como se ele fosse um gato, em 10 palavras. Acaba a miar:", "False");
-      }
-    }
-  }, intervalInMilliseconds);
-}
-
 function getDateTime() {
   var date = new Date();
   var hour = date.getHours();
